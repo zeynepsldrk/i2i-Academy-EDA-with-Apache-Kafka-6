bfd2f6ee1d8153f22918c28561670834d6f353ef
zeynepsldrk <162623674+zeynepsldrk@users.noreply.github.com>
2026-07-04 07:51:46 +0300

    Initial commit

---
+++ src/main/java/com/example/i2iacademyedawithapachekafka6/I2iAcademyEdaWithApacheKafka6Application.java
@@ -0,0 +1,13 @@
+package com.example.i2iacademyedawithapachekafka6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I2iAcademyEdaWithApacheKafka6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I2iAcademyEdaWithApacheKafka6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/i2iacademyedawithapachekafka6/I2iAcademyEdaWithApacheKafka6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.i2iacademyedawithapachekafka6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I2iAcademyEdaWithApacheKafka6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
